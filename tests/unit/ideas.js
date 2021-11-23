13e95d1426767eb3a094e094e3db21f876c6c263
OlegK5115 <olegk5115@gmail.com>
2021-11-23 22:17:32 +0300

    Added new tests for users and ideas

--- tests/unit/ideas.js
+++ tests/unit/ideas.js
@@ -4,186 +4,106 @@ const users = require('../../lib/users')
 
 // запуск тестов через npm test
 
-describe('Ideas', function() {
+describe('Ideas', function(){
+
+    const user = {
+        name : "Alex",
+        email : "alexf1989@example.com",
+        password : "Pitbuli32"
+    } 
+
+    const idea = {
+        "heading": "test",
+        "content": "This is my first idea"
+    }
+
     before(function() {
-      // очистка базы данных
         return ideas.setup()
         .then(() => {
-          return users.setup()
-          .then(() => {
-            return ideas.clearIdeas()
+            return users.setup()
             .then(() => {
-              return users.clearUsers()
+                return ideas.clearIdeas()
+                .then(() => {
+                    return users.clearUsers()
+                    .then(() => {
+                        return users.registration(user)
+                        .then((rezult) => {
+                            user.id = rezult.id
+                        })
+                    })
+                })
             })
-          })
         })
     })
-
+    
     context('There is no Ideas', function() {
-        it('Getting list of Ideas without parameters', function() {
-          return ideas.getAllIdeas()
+
+        it('Getting empty list of Ideas', function() {
+            return ideas.getAllIdeas()
             .then(ideas => {
-              ideas.should.be.an.instanceOf(Array).and.have.lengthOf(0)
+                ideas.should.be.an.instanceOf(Array).and.have.lengthOf(0)
             })
         })
-        // getideas с правильными параметрами
+
+        it('Adding first idea', function() {
+            return ideas.saveIdea(idea, user.email)
+            .then(rezult => {
+                rezult.should.have.property("status")
+                rezult.status.should.be.equal(true)
+                rezult.should.have.property("message")
+                rezult.should.have.property("id")
+                idea.id = rezult.id
+            })
+        })
+
+        it('Getting idea by id', function() {
+            return ideas.showIdea(idea.id)
+            .then(rezult => {
+                rezult.should.have.property("status")
+                rezult.status.should.be.equal(true)
+                rezult.should.have.property("heading")
+                rezult.heading.should.be.equal(idea.heading)
+                rezult.should.have.property("content")
+                rezult.content.should.be.equal(idea.content)
+                rezult.should.have.property("id")
+                rezult.id.should.be.equal(idea.id)
+            })
+        })
+
+        after(function() {
+            return ideas.clearIdeas()
+        })
+
     })
 
     // saveIdea, showIdea, getLenght
 
     context('There is one Idea', function() {
-      const user = {
-        name : "Alex",
-        email : "alexf1989@example.com",
-        password : "Pitbuli32"
-      } 
+        
 
-      const idea = {
-        "heading": "test",
-        "content": "This is my first idea"
-      }
-
-      before(function() {
-        return users.registration(user)
-        .then((rezult) => {
-          user.id = rezult.id
-          return ideas.saveIdea(idea, user.email)
-          .then(rezult => {
-            idea.id = rezult.id
-          })
+        before(function() {
+            return ideas.saveIdea(idea, user.email)
+            .then(rezult => {
+                idea.id = rezult.id
+            })
         })
-      })
 
-      it('Getting list of Ideas', function(){
-        return ideas.getAllIdeas()
-          .then(ideas => {
-            ideas.should.be.an.instanceOf(Array).and.have.lengthOf(1)
-            ideas[0].should.have.property("_id", idea.id)
-            ideas[0].should.have.property("heading", idea.heading)
-            ideas[0].should.have.property("content", idea.content)
-          })
-      })
-      // test для SaveIdea
-
-      after(function() {
-        return ideas.clearIdeas()
-        .then(() => {
-          return users.clearUsers()
+        it('Getting list of one Idea', function(){
+            return ideas.getAllIdeas()
+            .then(ideas => {
+                ideas.should.be.an.instanceOf(Array).and.have.lengthOf(1)
+                ideas[0].should.have.property("_id", idea.id)
+                ideas[0].should.have.property("heading", idea.heading)
+                ideas[0].should.have.property("content", idea.content)
+            })
         })
-      })
+
+        after(function() {
+            return ideas.clearIdeas()
+        })
     })
 
-    context('There are two Ideas', function() {
-      const user = {
-        name : "Alex",
-        email : "alexf1989@example.com",
-        password : "Pitbuli32"
-      }
-
-      const idea1 = {
-        "heading": "test",
-        "content": "This is my first idea"
-      }
-      const idea2 = {
-        "heading": "second test",
-        "content": "This is my second idea"
-      }
-      before(function() {
-        return users.registration(user)
-        .then((rezult) => {
-          user.id = rezult.id
-          return ideas.saveIdea(idea1, user.email)
-          .then(resOfIdea1 => {
-            idea1.id = resOfIdea1.id
-            return ideas.saveIdea(idea2, user.email)
-            .then(resOfIdea2 => {
-              idea2.id = resOfIdea2.id
-            })
-          })
-        })
-      })
-
-      
-      it('Support idea', function() {
-        return ideas.ideaUp(user.email, idea1.id)
-        .then(result => {
-          result.status.should.be.equal(true)
-          result.support.should.be.equal(1)
-        })
-        //  Тест Up идеи (повысить поддержку)
-      })
-
-      it('Support idea again', function() {
-        return ideas.ideaUp(user.email, idea1.id)
-        .then(result => {
-          result.status.should.be.equal(true)
-          result.support.should.be.equal(0)
-        })
-        //  Тест Up идеи (снять поддержку)
-      })
-
-      it('Flip up idea support', function() {
-        return ideas.ideaDown(user.email, idea1.id)
-        .then(() => {
-          return ideas.ideaUp(user.email, idea1.id)
-          .then(result => {
-            result.status.should.be.equal(true)
-            result.support.should.be.equal(1)
-          })
-        })
-        //  Тест Up идеи (сменить поддержку)
-      })
-      
-      it('Unsupport idea', function() {
-        return ideas.ideaDown(user.email, idea1.id)
-        .then(result => {
-          result.status.should.be.equal(true)
-          result.support.should.be.equal(-1)
-        })
-        // Тест Down идеи (понизить поддержку)
-      })
-      
-      it('Unsupport idea again', function() {
-        return ideas.ideaDown(user.email, idea1.id)
-        .then(result => {
-          result.status.should.be.equal(true)
-          result.support.should.be.equal(0)
-        })
-        //  Тест Down идеи (снять поддержку)
-      })
-
-      it('Flip down idea support', function() {
-        return ideas.ideaUp(user.email, idea1.id)
-        .then(() => {
-          return ideas.ideaDown(user.email, idea1.id)
-          .then(result => {
-            result.status.should.be.equal(true)
-            result.support.should.be.equal(-1)
-          })
-        })
-        //  Тест Up идеи (сменить поддержку)
-      })
-
-      it('Getting list of Ideas', function(){
-        return ideas.getAllIdeas()
-          .then(ideas => {
-            ideas.should.be.an.instanceOf(Array).and.have.lengthOf(2)
-            ideas[0].should.have.property("_id", idea2.id)
-            ideas[0].should.have.property("heading", idea2.heading)
-            ideas[0].should.have.property("content", idea2.content)
-            ideas[0].should.have.property("support", 0)
-            ideas[1].should.have.property("_id", idea1.id)
-            ideas[1].should.have.property("heading", idea1.heading)
-            ideas[1].should.have.property("content", idea1.content)
-            ideas[1].should.have.property("support", -1)
-          })
-      })
-
-      after(function() {
-        return ideas.clearIdeas()
-        .then(() => {
-          users.clearUsers()
-        })
-      })
+    after(function() {
+        return users.clearUsers()
     })
 })
--- tests/unit/users.js
+++ tests/unit/users.js
@@ -3,9 +3,14 @@ const users = require('../../lib/users')
 
 
 
-describe('Users and ideas', function() {
+describe('Users', function() {
+    const user = {
+        name : "Alex",
+        email : "alexf1989@example.com",
+        password : "Pitbuli32"
+    }
+
     before(function() {
-        // очистка базы данных
         return users.setup()
         .then(() => {
           return users.clearUsers()
@@ -13,21 +18,78 @@ describe('Users and ideas', function() {
     })
 
     context('There is no users', function() {
-        it('Getting an error when searching for a user', function() {
+
+        it('Getting empty list of users', function() {
             return users.getAllUsers()
             .then(users => {
                 users.should.be.an.instanceOf(Array).and.have.lengthOf(0)
             })
         })
 
+        it('Getting user by mail', function() {
+            return users.getUserByEmail(user.email)
+            .then(user => {
+                should(user).be.undefined
+            })
+        })
+
+        it('Trying signin user', function() {
+            return users.signin({email : user.email, password : user.password})
+            .then(result=> {
+                result.should.have.property('status')
+                result.status.should.be.equal(false)
+                result.should.have.property('message')
+                result.message.should.be.equal("Can't find user")
+            })
+        })
+
+    })
+
+    context('There is no users', function() {
+
+        it('Regirstration null', function() {
+            return users.registration(null)
+            .then(result => {
+                result.should.have.property('status')
+                result.status.should.be.equal(false)
+                result.should.have.property('message')
+            })
+        })
+
+        it('Regirstration user without name', function() {
+            return users.registration({email : user.email, password : user.password})
+            .then(result => {
+                result.should.have.property('status')
+                result.status.should.be.equal(false)
+                result.should.have.property('message')
+            })
+        })
+
+        it('Regirstration user', function() {
+            return users.registration(user)
+            .then(result => {
+                result.should.have.property('status')
+                result.status.should.be.equal(true)
+                result.should.have.property('message')
+                result.should.have.property('userid')
+            })
+        })
+
+        it('Regirstration user again', function() {
+            return users.registration(user)
+            .then(result => {
+                result.should.have.property('status')
+                result.status.should.be.equal(false)
+                result.should.have.property('message')
+            })
+        })
+
+        after(function() {
+            return users.clearUsers()
+        })
     })
 
     context('There is one user', function() {
-        const user = {
-            name : "Alex",
-            email : "alexf1989@gmail.com",
-            password : "Pitbuli32"
-        }
 
         before(function() {
            return users.registration(user)
@@ -61,12 +123,6 @@ describe('Users and ideas', function() {
 
         after(function() {
             return users.clearUsers()
-            .then(() => {
-                users.getAllUsers()
-                .then(mass => {
-                    console.log("Users after clean", mass)
-                })
-            })
         })
     })
 })
